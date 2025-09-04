f31975c2fe19c34cdb83c519475cecdd5ab002aa
Sahil Chaurasiya <chaurasiyasahil18@gmail.com>
2025-09-04 18:03:04 +0530

    first commit-2

--- RMS_app/urls.py
+++ RMS_app/urls.py
@@ -3,6 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.demo, name='home'),           # default page
-    path('analytics.html', views.analytics, name='analytics'),
-    path('battery.html', views.battery, name='battery'),
+    path('analytics', views.analytics, name='analytics'),
+    path('battery', views.battery, name='battery'),
+    path('admin', views.admin, name='admin'),
+    path('energy', views.energy, name='energy'),
+    path('grid_billing', views.grid_billing, name='grid_billing'),
+    path('login', views.login, name='login'),
+    path('maps', views.maps, name='maps'),
+    path('reports', views.reports, name='reports'),
+
 ]
--- RMS_app/views.py
+++ RMS_app/views.py
@@ -9,8 +9,23 @@ def analytics(request):
 def battery(request):
     return render(request, "battery.html")
 
-def analytics(request):
-    return render(request, "analytics.html")
+def admin(request):
+    return render(request, "admin.html")
+
+def energy(request):
+    return render(request, "energy.html")
+
+def grid_billing(request):
+    return render(request, "grid_billing.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def maps(request):
+    return render(request, "maps.html")
+
+def reports(request):
+    return render(request, "reports.html")
 
 
 

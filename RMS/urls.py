75ef618429fe120894911a8f0eed4521fc48ee36
Shivam774705 <kumarshivam774705@gmail.com>
2025-09-05 10:12:22 +0530

    fixed sidebar

--- RMS/urls.py
+++ RMS/urls.py
@@ -4,13 +4,13 @@ from RMS_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('analytics/', views.analytics, name='analytics'),
     path('battery/', views.battery, name='battery'),
     path('useradmin/', views.useradmin, name='useradmin'),
     path('energy/', views.energy, name='energy'),
     path('grid_billing/', views.grid_billing, name='grid_billing'),
-    path('', views.login, name='login'),
+    path('/login', views.login, name='login'),
     path('maps/', views.maps, name='maps'),
     path('reports/', views.reports, name='reports'),
 ]
